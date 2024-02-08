fa3388e754b644d77066d1474bd60dd69c9205fd
Dron245 <Dron245@yandex.ru>
2024-02-08 22:03:23 +0300

    0802-2203

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+	const targetElement = e.target;
+	const menuList =document.querySelector('.menu__list')
+	if(targetElement.closest('.menu__link')){
+		let menuItem = targetElement.closest('.menu__item')
+		targetElement.classList.add('_open')
+		menuItem.classList.add('_open')
+		menuList.classList.add('_sub-menu-open')
+	}
+
+}
