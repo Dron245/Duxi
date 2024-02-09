428e157d1de8c781c22bb971e5af24edd188ee9c
Dron245 <Dron245@yandex.ru>
2024-02-09 15:07:59 +0300

    0902-1507

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,12 +7,21 @@ document.addEventListener("click", documentActions);
 
 function documentActions(e) {
 	const targetElement = e.target;
+	console.log(targetElement);
 	const menuList =document.querySelector('.menu__list')
 	if(targetElement.closest('.menu__link')){
 		let menuItem = targetElement.closest('.menu__item')
-		targetElement.classList.add('_open')
-		menuItem.classList.add('_open')
-		menuList.classList.add('_sub-menu-open')
+		targetElement.classList.toggle('_open')
+		targetElement.closest('.menu__item').querySelector('.menu__sublist') ? 
+		targetElement.closest('.menu__item').querySelector('.menu__sublist').classList.toggle('_sub-menu-open') : null
+		// menuItem.classList.add('_open')
+		// menuList.classList.toggle('_sub-menu-open')
 	}
 
+	if(!targetElement.closest('.menu__body' )){
+		if(document.documentElement.classList.contains('menu-open')){
+			document.documentElement.classList.remove('menu-open')
+			document.documentElement.classList.remove('lock')
+		}
+	}
 }
