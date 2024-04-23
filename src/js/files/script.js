a86f2ffb49e41aaadef08fa188e0781c9b3b7c94
Dron245 <Dron245@yandex.ru>
2024-04-23 16:08:18 +0300

    2304-1608

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,7 +2,7 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
-
+import Sortable from 'sortablejs';
 window.addEventListener('DOMContentLoaded', () => {
 	document.addEventListener("click", documentActions);
 	let flag = true;
@@ -869,6 +869,45 @@ window.addEventListener('DOMContentLoaded', () => {
 				} 
 			});
 		}*/
+
+		//Перемещение объектов на странице "Избранное"
+		if (targetElement.closest('.favorite__button_redaction')) {
+			// drag();
+			new Sortable(example1, {
+				animation: 150,
+				ghostClass: 'background-class'
+			});
+			new Sortable(example2, {
+				animation: 150,
+				ghostClass: 'background-class'
+			});
+			targetElement.closest('.favorites__actions').classList.add('_change-button-favorites');
+			const buttonFavorite = document.querySelectorAll('.product__actions')
+			buttonFavorite.forEach(element => {
+				element.classList.add('_button-del')
+			});
+			targetElement.closest('[data-tabs]').querySelector('[data-tabs-body]').classList.add('_redaction-yes')
+		} else if(targetElement.closest('.favorite__button_save')) {
+			targetElement.closest('.favorites__actions').classList.remove('_change-button-favorites');
+			const buttonFavorite = document.querySelectorAll('.product__actions')
+			buttonFavorite.forEach(element => {
+				element.classList.remove('_button-del')
+			});
+			targetElement.closest('[data-tabs]').querySelector('[data-tabs-body]').classList.remove('_redaction-yes')
+		}
+		
+		//Удаляю продукты из избранного
+		if (targetElement.closest('.list .product-del-favorites')) {
+			targetElement.closest('.product').remove()
+			const removeItem = targetElement.closest('.product').dataset.list
+			const removeItemBlock = document.querySelector(`[data-block="${removeItem}"]`)
+			removeItemBlock.remove()
+		} else if(targetElement.closest('.tile .product-del-favorites')) {
+			targetElement.closest('.product').remove()
+			const removeItem = targetElement.closest('.product').dataset.block
+			const removeItemBlock = document.querySelector(`[data-list="${removeItem}"]`)
+			removeItemBlock.remove()
+		}
 	}
 });
 
@@ -1171,6 +1210,7 @@ new AirDatepicker('#lk-birthday', {
 
 
 
+
 //Преобразование фильтров-спойлеров в каталоге в таблетном разрешении
 const catalog = document.querySelector('.catalog');
 if (catalog) {
@@ -1181,7 +1221,7 @@ if (catalog) {
 	// console.log(spollersWrapper);
 	if (window.innerWidth < 1001) {
 		details.forEach(element => {
-			console.log(1);
+			// console.log(1);
 			element.removeAttribute('data-open');
 		});
 		spollersWrapper.setAttribute('data-one-spoller', '')
@@ -1215,3 +1255,144 @@ if(store) {
 		store.querySelector('.store-not-open').setAttribute('data-open', '')
 	}
 }
+
+
+
+
+
+/*function drag() {
+	var dragSrcEl = null;
+	
+	function handleDragStart(e) {
+	
+	//   this.style.opacity = '0.4';
+	  
+	  dragSrcEl = this;
+ 
+	  e.dataTransfer.effectAllowed = 'move';
+	  e.dataTransfer.setData('text/html', this.innerHTML);
+	}
+ 
+	function handleDragOver(e) {
+	  if (e.preventDefault) {
+		 e.preventDefault();
+	  }
+ 
+	  e.dataTransfer.dropEffect = 'move';
+	  
+	  return false;
+	}
+ 
+	function handleDragEnter(e) {
+	  this.classList.add('over');
+	}
+ 
+	function handleDragLeave(e) {
+	  this.classList.remove('over');
+	}
+ 
+	function handleDrop(e) {
+	  if (e.stopPropagation) {
+		 e.stopPropagation(); // stops the browser from redirecting.
+	  }
+	  
+	  if (dragSrcEl != this) {
+		 dragSrcEl.innerHTML = this.innerHTML;
+		 this.innerHTML = e.dataTransfer.getData('text/html');
+	  }
+	  
+	  return false;
+	}
+ 
+	function handleDragEnd(e) {
+	  this.style.opacity = '1';
+	  
+	  items.forEach(function (item) {
+		 item.classList.remove('over');
+	  });
+	}
+	
+	
+	let items = document.querySelectorAll('.favorite__wrapper .box');
+	items.forEach(function(item) {
+	  item.addEventListener('dragstart', handleDragStart, false);
+	  item.addEventListener('dragenter', handleDragEnter, false);
+	  item.addEventListener('dragover', handleDragOver, false);
+	  item.addEventListener('dragleave', handleDragLeave, false);
+	  item.addEventListener('drop', handleDrop, false);
+	  item.addEventListener('dragend', handleDragEnd, false);
+	});
+}*/
+
+/*document.addEventListener('DOMContentLoaded', (event) => {
+	// const favorites = document.querySelector('.favorites')
+	// const imgFavorite = favorites.querySelectorAll('.product__img > img')
+	// console.log(imgFavorite);
+	// imgFavorite.forEach(element => {
+	// 	element.addEventListener('dragover', function(e) {
+	// 		console.log('123');
+	// 		e.preventDefault()
+	// 	})
+	// });
+	var dragSrcEl = null;
+	
+	function handleDragStart(e) {
+	
+	//   this.style.opacity = '0.4';
+	  
+	  dragSrcEl = this;
+ 
+	  e.dataTransfer.effectAllowed = 'move';
+	  e.dataTransfer.setData('text/html', this.innerHTML);
+	}
+ 
+	function handleDragOver(e) {
+	  if (e.preventDefault) {
+		 e.preventDefault();
+	  }
+ 
+	  e.dataTransfer.dropEffect = 'move';
+	  
+	  return false;
+	}
+ 
+	function handleDragEnter(e) {
+	  this.classList.add('over');
+	}
+ 
+	function handleDragLeave(e) {
+	  this.classList.remove('over');
+	}
+ 
+	function handleDrop(e) {
+	  if (e.stopPropagation) {
+		 e.stopPropagation(); // stops the browser from redirecting.
+	  }
+	  
+	  if (dragSrcEl != this) {
+		 dragSrcEl.innerHTML = this.innerHTML;
+		 this.innerHTML = e.dataTransfer.getData('text/html');
+	  }
+	  
+	  return false;
+	}
+ 
+	function handleDragEnd(e) {
+	  this.style.opacity = '1';
+	  
+	  items.forEach(function (item) {
+		 item.classList.remove('over');
+	  });
+	}
+	
+	
+	let items = document.querySelectorAll('.favorite__wrapper .box');
+	items.forEach(function(item) {
+	  item.addEventListener('dragstart', handleDragStart, false);
+	  item.addEventListener('dragenter', handleDragEnter, false);
+	  item.addEventListener('dragover', handleDragOver, false);
+	  item.addEventListener('dragleave', handleDragLeave, false);
+	  item.addEventListener('drop', handleDrop, false);
+	  item.addEventListener('dragend', handleDragEnd, false);
+	});
+ });*/
